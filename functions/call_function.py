ab0693c4eb12a0b9d05cbc7f0c7fe687c9aa75b1
Dosu333 <vladamir1865@gmail.com>
2025-11-05 08:26:12 +0100

    Add cart and order function calls to call_function.py for enhanced functionality

--- functions/call_function.py
+++ functions/call_function.py
@@ -1,4 +1,7 @@
 from functions.wallet import make_payment
+from functions.cart import (add_to_cart, view_cart, clear_cart,
+                            remove_from_cart, edit_cart)
+from functions.orders import get_orders
 from google.genai import types
 
 
@@ -12,6 +15,18 @@ def call_function(function_call_part, verbose=True, user_id=None):
     result = ""
     if function_call_part.name == "make_payment":
         result = make_payment(user_id=user_id, **function_call_part.args)
+    elif function_call_part.name == "add_to_cart":
+        result = add_to_cart(**function_call_part.args)
+    elif function_call_part.name == "view_cart":
+        result = view_cart()
+    elif function_call_part.name == "clear_cart":
+        result = clear_cart()
+    elif function_call_part.name == "remove_from_cart":
+        result = remove_from_cart(**function_call_part.args)
+    elif function_call_part.name == "edit_cart":
+        result = edit_cart(**function_call_part.args)
+    elif function_call_part.name == "get_orders":
+        result = get_orders(user_id=user_id)
     else:
         result = {"status": "failure", "message": "Function not found"}
 
